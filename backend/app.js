c6c59d6651162f024aed1b42aee7bb2b8a2ea5c8
Ramkrishna Mahato <rkmahato010@gmail.com>
2023-05-31 16:34:26 +0530

    blog

--- backend/app.js
+++ backend/app.js
@@ -4,83 +4,84 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-var cors = require('cors');
-var cookieParser = require('cookie-parser');
-const path = require('path');
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+const path = require("path");
 
 //adding socket.io configuration
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const errorHandler = require('./middleware/error');
+const errorHandler = require("./middleware/error");
 
 //import routes
-const authRoutes = require('./routes/authRoutes');
-const postRoute = require('./routes/postRoute');
+const authRoutes = require("./routes/authRoutes");
+const postRoute = require("./routes/postRoute");
 const { Socket } = require("dgram");
 
 //database connection
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-})
-    .then(() => console.log("DB connected"))
-    .catch((err) => console.log(err));
-
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 //MIDDLEWARE
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "5mb" }));
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     limit: "5mb",
-    extended: true
-}));
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(cors());
 
 //ROUTES MIDDLEWARE
-app.use('/api', authRoutes);
-app.use('/api', postRoute);
+app.use("/api", authRoutes);
+app.use("/api", postRoute);
 
-__dirname = path.resolve()
+__dirname = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
 }
-
 
 //error middleware
 app.use(errorHandler);
 
 //port
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
 
 // app.listen(port, () => {
 //     console.log(` Server running on port ${port}`);
 // })
-io.on('connection', (socket) => {
-    //console.log('a user connected', socket.id);
-    socket.on('comment', (msg) => {
-        // console.log('new comment received', msg);
-        io.emit("new-comment", msg);
-    })
-})
 
-exports.io = io
+io.on("connection", (socket) => {
+  // console.log("a user connected", socket.id);
+  socket.on("comment", (msg) => {
+    // console.log("new comment received", msg);
+    io.emit("new-comment", msg);
+  });
+});
+
+exports.io = io;
 
 server.listen(port, () => {
-    console.log(` Server running on port ${port}`);
-})
-
+  console.log(` Server running on port ${port}`);
+});
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,22 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 const Footer = () => {
-    return (
-        <>
-            <Box sx={{ bgcolor: "rgba(0,0,0,.7)", height: "60px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <Box sx={{ color: "#fafafa" }}>Footer</Box>
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "black",
+          height: "60px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ color: "#fafafa" }}>Footer</Box>
+      </Box>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
